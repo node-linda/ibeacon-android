b5c10038d97d186f5b761a2bc85498c5705699a2
Sho Hashimoto <hashimoto@shokai.org>
2014-05-21 14:57:54 +0900

    create project

---
+++ gen/org/shokai/linda/ibeacon/BuildConfig.java
@@ -0,0 +1,8 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.shokai.linda.ibeacon;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
---
+++ src/org/shokai/linda/ibeacon/MainActivity.scala
@@ -0,0 +1,13 @@
+package org.shokai.linda.ibeacon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+class MainActivity extends Activity{
+
+  override def onCreate(savedInstanceState:Bundle){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+
+}
