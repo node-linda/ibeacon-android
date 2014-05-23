896798ebcf0305e469f47337b28c86897324cd14
Sho Hashimoto <hashimoto@shokai.org>
2014-05-23 14:52:16 +0900

    notify at outside of Region

--- src/org/shokai/linda/ibeacon/LindaService.scala
+++ src/org/shokai/linda/ibeacon/LindaService.scala
@@ -12,6 +12,9 @@ class LindaService extends Service{
   lazy val iBeacon:IBeacon = new IBeacon(this)
   lazy val notifer:Notifer = new Notifer(appName, this)
   val door = new Door()
+  val uuid = "B0FC4601-14A6-43A1-ABCD-CB9CFDDB4013"
+  val major = "0001"
+  val minor = "0094"
 
   var handler:Handler = null
 
@@ -23,8 +26,12 @@ class LindaService extends Service{
     val looper = thread.getLooper()
     handler = new Handler(looper){
       override def handleMessage(msg:Message){
-        iBeacon.onRegion("B0FC4601-14A6-43A1-ABCD-CB9CFDDB4013", "0001", "0094", Range(-70, 0), (beacon:Beacon) => {
-          print(s"door open: UUID=${beacon.uuid} Major=${beacon.major} Minor=${beacon.minor} RSSI=${beacon.rssi}")
+        iBeacon.onRegion(uuid, major, minor, Range(-90, -70), (beacon:Beacon) => {
+          print(s"outside of DeltaS112 (RSSI:${beacon.rssi})")
+        })
+
+        iBeacon.onRegion(uuid, major, minor, Range(-70, 0), (beacon:Beacon) => {
+          print(s"door open DeltaS112 (RSSI:${beacon.rssi})")
           door.open()
         })
       }
