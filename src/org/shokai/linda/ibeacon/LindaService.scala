0bc56f3e010b91ae95bc04efad05a64f2417351f
Sho Hashimoto <hashimoto@shokai.org>
2014-05-24 04:48:06 +0900

    save RSSI threshold by SeekBar on MainActivity  #1
    use threshold in SharedPreferences on LindaService #1

--- src/org/shokai/linda/ibeacon/LindaService.scala
+++ src/org/shokai/linda/ibeacon/LindaService.scala
@@ -3,7 +3,7 @@ package org.shokai.linda.ibeacon;
 import org.shokai.ibeacon.{IBeacon, Beacon};
 import android.app.{Service, IntentService};
 import android.os.{Bundle, IBinder, Looper, Handler, HandlerThread, Process, Message};
-import android.content.Intent;
+import android.content.{Context, Intent, SharedPreferences};
 import android.util.Log;
 
 class LindaService extends Service{
@@ -12,6 +12,11 @@ class LindaService extends Service{
   lazy val iBeacon:IBeacon = new IBeacon(this)
   lazy val notifer:Notifer = new Notifer(appName, this)
   val door = new Door()
+  lazy val rssiRange:Range = Range(getResources().getInteger(R.integer.rssi_min),
+                                   getResources().getInteger(R.integer.rssi_max))
+  lazy val pref:SharedPreferences = getSharedPreferences(appName, Context.MODE_PRIVATE)
+  lazy val threshold:Int = pref.getInt("threshold", rssiRange(rssiRange.size/2))
+
   val uuid = "B0FC4601-14A6-43A1-ABCD-CB9CFDDB4013"
   val major = "0001"
   val minor = "0094"
@@ -19,18 +24,18 @@ class LindaService extends Service{
   var handler:Handler = null
 
   override def onCreate{
-    print("start service")
+    print(s"start service (threshold:${threshold})")
 
     var thread:HandlerThread = new HandlerThread(appName, Process.THREAD_PRIORITY_BACKGROUND)
     thread.start()
     val looper = thread.getLooper()
     handler = new Handler(looper){
       override def handleMessage(msg:Message){
-        iBeacon.onRegion(uuid, major, minor, Range(-90, -70), (beacon:Beacon) => {
+        iBeacon.onRegion(uuid, major, minor, Range(-90, threshold), (beacon:Beacon) => {
           print(s"outside of DeltaS112 (RSSI:${beacon.rssi})")
         })
 
-        iBeacon.onRegion(uuid, major, minor, Range(-70, 0), (beacon:Beacon) => {
+        iBeacon.onRegion(uuid, major, minor, Range(threshold, 0), (beacon:Beacon) => {
           print(s"door open DeltaS112 (RSSI:${beacon.rssi})")
           door.open()
         })
--- src/org/shokai/linda/ibeacon/MainActivity.scala
+++ src/org/shokai/linda/ibeacon/MainActivity.scala
@@ -2,28 +2,40 @@ package org.shokai.linda.ibeacon;
 
 import android.app.{Activity, PendingIntent};
 import android.os.Bundle;
-import android.content.{Context, Intent};
-import android.widget.Button;
+import android.content.{Context, Intent, SharedPreferences};
+import android.widget.{Button, SeekBar, TextView};
 import android.view.View;
 import android.util.Log;
 
 class MainActivity extends Activity{
+  import scala.collection.immutable.Range;
 
   lazy val appName:String = getResources().getString(R.string.app_name)
   lazy val btnStart:Button = findViewById(R.id.btnStart).asInstanceOf[Button]
   lazy val btnStop:Button = findViewById(R.id.btnStop).asInstanceOf[Button]
   lazy val serviceIntent:Intent = new Intent(this, classOf[LindaService])
 
+  lazy val pref:SharedPreferences = getSharedPreferences(appName, Context.MODE_PRIVATE)
+  lazy val prefEdit:SharedPreferences.Editor = pref.edit()
+  lazy val rssiRange:Range = Range(getResources().getInteger(R.integer.rssi_min),
+                                   getResources().getInteger(R.integer.rssi_max))
+  lazy val seekBarRssi:SeekBar = findViewById(R.id.seekBarRssi).asInstanceOf[SeekBar]
+  lazy val textViewRssi:TextView = findViewById(R.id.textViewRssi).asInstanceOf[TextView]
+
   override def onCreate(savedInstanceState:Bundle){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    print("app start")
+    var threshold:Int = pref.getInt("threshold", rssiRange(rssiRange.size/2))
+
+    print(s"app start (threshold:${threshold})")
 
     val self = this
 
     btnStart.setOnClickListener( new View.OnClickListener(){
       override def onClick(v:View){
+        prefEdit.putInt("threshold", threshold)
+        prefEdit.commit()
         self.startService(serviceIntent)
       }
     })
@@ -33,6 +45,22 @@ class MainActivity extends Activity{
         self.stopService(serviceIntent)
       }
     })
+
+    textViewRssi.setText(s"Beacon RSSI Threshold : ${threshold}")
+    seekBarRssi.setMax(rssiRange.max - rssiRange.min + 2)
+    seekBarRssi.setProgress(threshold - rssiRange.min)
+
+    seekBarRssi.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener(){
+      override def onProgressChanged(seekBar:SeekBar, value:Int, fromTouch:Boolean){
+        threshold = rssiRange.min+value
+        textViewRssi.setText(s"Beacon RSSI Threshold : ${threshold}")
+      }
+      override def onStartTrackingTouch(seekBar:SeekBar){
+      }
+      override def onStopTrackingTouch(seekBar:SeekBar){
+      }
+    })
+
   }
 
   def print(msg:String){
